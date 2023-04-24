3cfffdbfc8595f57bb703ab5e5795efbc45c694a
akatimer <akatimer@gmail.com>
2023-04-24 15:59:55 +0300

    fix: repair functionality of new task and using BEM rule 2.1 html-and-css-extended.md

--- app.js
+++ app.js
@@ -32,19 +32,22 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className='app-body__item';
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='app-body__label task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="app-body__task task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="app-body__button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="app-body__button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='app-body__img-remove';
     deleteButton.appendChild(deleteButtonImg);
 
 
